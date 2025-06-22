43c6f13774bb0a171646bc410c881e38625d6cbd
Ryan Brock <rbrock444@gmail.com>
2025-06-21 19:08:56 -0500

    tweak classes

--- src/objects/connect4_a_i.ts
+++ src/objects/connect4_a_i.ts
@@ -1,16 +1,13 @@
 import type {GameState} from "./index";
 
-export class Connect4AI {
+export abstract class Connect4AI {
     player: number;
     opponent: number;
 
-    constructor(player = 2) {
+    protected constructor(player = 2) {
         this.player = player;
         this.opponent = player === 1 ? 2 : 1;
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    makeMove(gameState: GameState) {
-        throw new Error('makeMove must be implemented by subclass');
-    }
+    abstract makeMove(gameState: GameState): number[][];
 }
